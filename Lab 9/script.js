6b8dafb6b1d8d088ee2963a671156a314c1dd530
hurleyms <hurleyms94@gmail.com>
2025-10-23 10:52:06 -0400

    lab 9.2

--- Lab 9/script.js
+++ Lab 9/script.js
@@ -19,3 +19,67 @@ while(i<=5){
 }
 document.getElementById("output").innerHTML = text;
 }
+
+function runDoWhileLoop(){
+    let text = "Counting using do-while loop:";
+    let i=1;
+    do{
+        text += i + " ";
+        i++;
+    }while (i<=5);
+// text = "counting using do-while loop: 1 2 3 4 5";
+document.getElementById("output").innerHTML = text;
+}
+
+function runIfElse(){
+    let text = "checking even or odd numbers:<br>";
+
+    for(let i=1;i<=5 ;i++){
+
+        if(i %2 === 0){
+
+            text += i + " is even<br>";
+
+        } else {
+            text += i + " is odd<br>";
+        }
+    }
+document.getElementById("output").innerHTML = text;
+
+let text1 = "checking next values for even and odd (10 to 15):<br>";
+
+for(let i=10;i<=15 ;i++){
+
+        if(i %2 === 0){
+
+            text1 += i + " is even<br>";
+
+        } else {
+            text1 += i + " is odd<br>";
+        }
+    }
+document.getElementById("output").innerHTML = text1;
+}
+
+function runSwitch(){
+    let grade = "B";
+    let message = "";
+
+    switch(grade){
+        case "A":
+            message = "Excellent";
+            break;
+        case "B":
+            message = "Good Job!";
+            break;
+        case "C":
+            message = "Well Done";
+            break;
+        case "D":
+            message = "Life is Hard";
+            break;
+        default:
+            message = "No recongnized grade";
+    }
+document.getElementById("output").innerHTML = "Switch statement: " + message;
+}
